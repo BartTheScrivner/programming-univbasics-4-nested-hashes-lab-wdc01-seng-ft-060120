2c57935bf7bf7cbddb40781ca266bb2c18a85f33
fantastic-scope-1514 <dframe22@gmail.com>
2020-04-06 04:18:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-    programmer_hash[:alan_kay][:known_for]
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -81,7 +81,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:yukihiro_matsumoto] = {
+		:known_for => "Ruby",
+		:languages => ["LISP", "C"]
+	}
 end
 
 def changing_alan
@@ -103,7 +106,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
